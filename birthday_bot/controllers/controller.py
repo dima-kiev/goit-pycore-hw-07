80a8cf48c08e26a8ebacc57b5fafc1eb8a025c79
dshk <dima.kiev@gmail.com>
2024-08-11 17:11:07 +0300

    [MCS07] HW 07

--- birthday_bot/controllers/controller.py
+++ birthday_bot/controllers/controller.py
@@ -2,7 +2,7 @@ class Controller:
     def __init__(self, cmd_factory):
         self.cmd_service = cmd_factory
 
-    def consume(self, user_inp):
+    def dispatch_command(self, user_inp):
         cmd_line = user_inp.strip().lower()
         command = self.cmd_service.find_command(cmd_line)
         return command.apply(cmd_line)
--- birthday_bot/views/view.py
+++ birthday_bot/views/view.py
@@ -4,7 +4,7 @@ class View:
         self.controller = controller
 
     def start(self, init_command):
-        print(self.controller.consume(init_command))
+        print(self.controller.dispatch_command(init_command))
         while True:
             prob_command = input("wats`up, Doc? ")
-            print(self.controller.consume(prob_command))
+            print(self.controller.dispatch_command(prob_command))
